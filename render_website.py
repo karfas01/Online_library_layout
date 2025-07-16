d21207a3c94483cc2f9bf62867d49420cddadf88
karfas <burglary773@gmail.com>
2025-07-16 19:42:45 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import os
 import json
 from livereload import Server
 from more_itertools import chunked
@@ -5,29 +6,37 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def on_reload():
+    os.makedirs('pages', exist_ok=True)
+
     with open('meta_data.json', encoding='utf-8') as f:
         books = json.load(f)
-    
-    chunked_books = list(chunked(books, 2))
+
+    pages_books = list(chunked(books, 15))
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=True
     )
+
     template = env.get_template('template.html')
-    rendered_page = template.render(  
-        chunked_books=chunked_books
-    )
-    
-    with open('index.html', 'w', encoding='utf-8') as f:
-        f.write(rendered_page)
+
+    for number, books_per_page in enumerate(pages_books, start=1):
+        chunked_books = list(chunked(books_per_page, 2))
+        
+        rendered_page = template.render(
+            chunked_books=chunked_books
+        )
+
+        output_path = f'pages/index{number}.html'
+        with open(output_path, 'w', encoding='utf-8') as f:
+            f.write(rendered_page)
 
 
 def main():
     on_reload()
     server = Server()
     server.watch("template.html", on_reload)
-    server.serve(root='.', open_url=True)
-
+    server.serve(root=".", open_url="/pages/index1.html")
 
 if __name__ == "__main__":
     main()
